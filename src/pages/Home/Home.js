1cca0022d3ebe52867a4ad8c1c8480e3b6ba10c0
Indo Karya Makmur <indokaryamakmur@Indos-MacBook-Pro.local>
2023-06-08 15:55:19 +0700

    Home - Info Feature

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../components/Home/Banner'
 import Why from '../../components/Home/Why'
+import Info from '../../components/Home/Info'
 
 const Home = () => {
   return (
     <>
     <Banner />
     <Why />
+    <Info />
     </>
   )
 }
