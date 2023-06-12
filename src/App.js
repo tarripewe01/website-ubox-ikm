8fdd9b70c29ef32f183c334e98a366d10ee7f604
tarripewe01 <tarripewe01@gmail.com>
2023-06-12 19:37:42 +0700

    Adding Privacy Policy Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </>
